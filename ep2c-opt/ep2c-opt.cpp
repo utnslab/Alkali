c73a8234cc232698bc76531fdb27f5609a3a14a1
Zhiyuan Guo <depctg@gmail.com>
2023-10-14 09:59:39 -0700

    move dialect defination to lib

---
+++ ep2c-opt/ep2c-opt.cpp
@@ -0,0 +1,41 @@
+#include "mlir/Dialect/Func/Extensions/AllExtensions.h"
+#include "ep2/lang/Parser.h"
+#include "ep2/dialect/Dialect.h"
+#include "ep2/dialect/MLIRGen.h"
+#include "ep2/dialect/Passes.h"
+
+#include "mlir/Dialect/Affine/Passes.h"
+#include "mlir/Dialect/LLVMIR/Transforms/Passes.h"
+#include "mlir/ExecutionEngine/ExecutionEngine.h"
+#include "mlir/ExecutionEngine/OptUtils.h"
+#include "mlir/IR/AsmState.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Verifier.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/Parser/Parser.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Export.h"
+#include "mlir/Transforms/Passes.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "mlir/InitAllPasses.h"
+
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ErrorOr.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/raw_ostream.h"
+ 
+int main(int argc, char **argv) {
+  mlir::DialectRegistry registry;
+  mlir::registerAllDialects(registry);
+  registry.insert<mlir::ep2::EP2Dialect>();
+ 
+  mlir::PassRegistration<mlir::ep2::NopEliminationPass>();
+ 
+  return mlir::asMainReturnCode(
+      mlir::MlirOptMain(argc, argv, "Tutorial Pass Driver", registry));
+}
---
+++ lib/ep2/NopEliminationPass.cpp
@@ -0,0 +1,26 @@
+#include "mlir/IR/BuiltinDialect.h"
+#include "ep2/dialect/Dialect.h"
+#include "ep2/dialect/Passes.h"
+
+#include "mlir/Pass/Pass.h"
+#include "mlir/IR/BuiltinOps.h"
+
+using namespace mlir;
+
+namespace mlir {
+namespace ep2 {
+
+void NopEliminationPass::runOnOperation() {
+    auto module = getOperation();
+    auto &context = getContext();
+    // Fill
+}
+
+std::unique_ptr<Pass> createNopEliminationPass() {
+    return std::make_unique<NopEliminationPass>();
+}
+
+}
+}
+
+
