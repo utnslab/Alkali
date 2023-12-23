ed1dce5b784f1d35deff3b834f1bef3bba5ab3a3
Jiaxin Lin <jxlin@utexas.edu>
2023-12-23 10:40:23 -0600

    update script

--- runfpga.sh
+++ runfpga.sh
@@ -4,5 +4,7 @@ set -e
 
 OUT_FILE_NAME="out-${1%.*}"
 
+ninja -C build/
 ./build/bin/ep2c $1 --emit=mlir -o "./tmp.mlir" 
 ./build/bin/ep2c-opt -canonicalize -ep2-context-infer -ep2-emit-fpga -o "tmpopt.mlir" "tmp.mlir"
+#./build/bin/ep2c-opt -canonicalize -ep2-context-infer -o "tmpopt.mlir" "tmp.mlir"
