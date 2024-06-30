ef33d2fb4b69ccca3fadde947405824c166d31d2
Zhiyuan Guo <depctg@gmail.com>
2024-06-30 16:15:42 -0700

    add polygeist interaces to ep2c-opt

--- ep2c-opt/ep2c-opt.cpp
+++ ep2c-opt/ep2c-opt.cpp
@@ -26,16 +26,29 @@
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "mlir/InitAllPasses.h"
 
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/ErrorOr.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/raw_ostream.h"
+
+// polygeist helper class
+class MemRefInsider
+    : public mlir::MemRefElementTypeInterface::FallbackModel<MemRefInsider> {};
+
+template <typename T>
+struct PtrElementModel
+    : public mlir::LLVM::PointerElementTypeInterface::ExternalModel<
+          PtrElementModel<T>, T> {};
  
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   mlir::registerAllDialects(registry);
   registry.insert<mlir::ep2::EP2Dialect>();
+  registry.insert<mlir::polygeist::PolygeistDialect>();
 
   mlir::PassRegistration<mlir::ep2::ArchMappingPass>();
   mlir::PassRegistration<mlir::ep2::BufferToValuePass>();
@@ -66,9 +79,29 @@ int main(int argc, char **argv) {
   mlir::PassRegistration<mlir::ep2::FPGABufferToStoragePass>();
   mlir::PassRegistration<mlir::ep2::ControllerGenerationPass>();
   mlir::PassRegistration<mlir::ep2::GlobalToPartitionPass>();
-  
 
   mlir::registerAllPasses();
+
+  // Polygeist to MLIR extensions
+  registry.addExtension(+[](mlir::MLIRContext *ctx, mlir::LLVM::LLVMDialect *dialect) {
+    mlir::LLVM::LLVMFunctionType::attachInterface<MemRefInsider>(*ctx);
+  });
+  registry.addExtension(+[](mlir::MLIRContext *ctx, mlir::LLVM::LLVMDialect *dialect) {
+    mlir::LLVM::LLVMArrayType::attachInterface<MemRefInsider>(*ctx);
+  });
+  registry.addExtension(+[](mlir::MLIRContext *ctx, mlir::LLVM::LLVMDialect *dialect) {
+    mlir::LLVM::LLVMPointerType::attachInterface<MemRefInsider>(*ctx);
+  });
+  registry.addExtension(+[](mlir::MLIRContext *ctx, mlir::LLVM::LLVMDialect *dialect) {
+    mlir::LLVM::LLVMStructType::attachInterface<MemRefInsider>(*ctx);
+  });
+  registry.addExtension(+[](mlir::MLIRContext *ctx, mlir::memref::MemRefDialect *dialect) {
+    mlir::MemRefType::attachInterface<PtrElementModel<mlir::MemRefType>>(*ctx);
+  });
+  registry.addExtension(+[](mlir::MLIRContext *ctx, mlir::LLVM::LLVMDialect *dialect) {
+    mlir::LLVM::LLVMStructType::attachInterface<
+        PtrElementModel<mlir::LLVM::LLVMStructType>>(*ctx);
+  });
  
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "EP2C transformation and generation driver", registry));
